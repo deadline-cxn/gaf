de4c93ec90abcc95d499e2e6e5eb3bf60222eaba
Seth Parson <seth.parson.71@gmail.com>
2020-04-27 03:58:33 -0400

    update

--- gaf.cpp
+++ gaf.cpp
@@ -1,35 +1,65 @@
-// GAF TOOL
+/***************************************************************
+ **   DLSTORM
+ **          /\
+ **   ---- D/L \----
+ **       \/
+ **   License:      BSD
+ **   Copyright:    2020
+ **   File:         gaf.cpp
+ **   Description:  GAF manager
+ **   Author:       Seth Parson aka Deadline
+ **   Twitter:      @Sethcoder
+ **   Website:      www.sethcoder.com
+ **   Email:        defectiveseth@gmail.com
+ **
+ ***************************************************************/
 
-#define GAFMAKEVERSION "4.0.1"
-#include "gaf_make.h"
+#include "gaf.h"
 
 CGAF *pGAF;
+char  szTopCWD[1024];
+char  szCWD[1024];
+char  szDir[1024];
 
 GAF_SCANCALLBACK dir_callback(GAFFile_ElmHeader *ElmInfo, LPSTR FullPath) {
     switch (ElmInfo->Type) {
-        case GAFELMTYPE_FILE: printf("%20d %-25s \n", ElmInfo->FileSize, ElmInfo->Name); break;
+        case GAFELMTYPE_FILE: printf("%20ld %-25s \n", ElmInfo->FileSize, ElmInfo->Name); break;
+        case GAFELMTYPE_DIR: printf("====== DIR: %-25s \n", ElmInfo->Name); break;
+        default: break;
+    }
+    return 0;
+}
 
+GAF_SCANCALLBACK dir_callback_extract(GAFFile_ElmHeader *ElmInfo, LPSTR FullPath) {
+    switch (ElmInfo->Type) {
+        case GAFELMTYPE_FILE:
+            printf("%20ld %s/%-25s \n", ElmInfo->FileSize, szDir, ElmInfo->Name);  // wut
+            pGAF->ExtractFile_ToFile(va("%s/%s", szDir, ElmInfo->Name), ElmInfo->Name);
+            break;
         case GAFELMTYPE_DIR:
             printf("====== DIR: %-25s \n", ElmInfo->Name);
-            // pGAF->ScanTree(ElmInfo->Name, (GAF_SCANCALLBACK) dir_callback);
+            strcpy(szDir, ElmInfo->Name);
+            dlcs_chdir(szTopCWD);
+            if (!dlcs_isdir(szDir)) dlcs_mkdir(szDir);
+            dlcs_chdir(szDir);
             break;
-
         default: break;
     }
     return 0;
 }
 
 int main(int argc, char *argv[]) {
+    setvbuf(stdout, NULL, _IONBF, 0);
     char file[1024];
     memset(file, 0, 1024);
     strcpy(file, "gaf.gaf");
     char res[1024];
     memset(res, 0, 1024);
     unsigned int i;
+    memset(szTopCWD, 0, 1024);
+    strcpy(szTopCWD, dlcs_getcwd());
 
     pGAF = new CGAF();
-
-    // remove(file);
 
     if (argc < 3) {
         print_help();
@@ -79,23 +109,31 @@ int main(int argc, char *argv[]) {
             pGAF->Close();
             break;
 
-        case 'x': printf("EXTRACT\n"); break;
-
-        case 'r': printf("REBUILD\n"); break;
+        case 'x':
+            printf("EXTRACT\n");
+            printf("Working in %s\n", szTopCWD);
+            memset(szDir, 0, 1024);
+            memset(szCWD, 0, 1024);
+            dlcs_chdir(szTopCWD);
+            pGAF->Open(file);
+            pGAF->ScanTree("", (GAF_SCANCALLBACK)dir_callback_extract);
+            pGAF->Close();
+            break;
 
         case 'l':
             printf("LIST CONTENTS\n");
-
             pGAF->Open(file);
             pGAF->ScanTree("", (GAF_SCANCALLBACK)dir_callback);
             pGAF->Close();
-
             break;
 
-        default: printf("??\n"); break;
+        default:
+            printf("Unknown or missing command. \n");
+            print_help();
+            break;
     }
 
-    dlcsm_delete(pGAF);
+    DEL(pGAF);
 
     return 0;
 }
@@ -108,12 +146,11 @@ void print_help(void) {
     printf("VERSION: %s compiled %s @ %s \n", GAFMAKEVERSION, __DATE__, __TIME__);
     printf("=======================================================\n");
     printf("USAGE:\n");
-    printf("gaf <gaf file> [-a:<filename>] [-z:<dir>] [-d:<file>] [-c:<dir>] [-x] [-r]\n");
-    printf("-a add a file to the GAF\n");
-    printf("-z add directory to the GAF\n");
+    printf("gaf <gaf file> [-a:<filename>] [-z:<dir>] [-d:<file>] [-c:<dir>] [-x]\n");
+    printf("-a add a file to the GAF (This will create a new GAF if it does not exist)\n");
+    printf("-z add directory to the GAF (This will create a new GAF if it does not exist)\n");
     printf("-d remove a file from the GAF\n");
     printf("-c remove a directory from the GAF\n");
     printf("-x extract GAF into the working directory\n");
     printf("-l list contents\n");
-    printf("-r rebuild the gaf\n");
 }
--- gaf.h
+++ gaf.h
@@ -1,5 +1,25 @@
-#define _DLCS_CONSOLE
-#include "dlstorm.h"
+
+/***************************************************************
+ **   DLSTORM
+ **          /\
+ **   ---- D/L \----
+ **       \/
+ **   License:      BSD
+ **   Copyright:    2020
+ **   File:         gaf.cpp
+ **   Description:  GAF manager
+ **   Author:       Seth Parson aka Deadline
+ **   Twitter:      @Sethcoder
+ **   Website:      www.sethcoder.com
+ **   Email:        defectiveseth@gmail.com
+ **
+ **   Note: You must define _DLCS_CONSOLE
+ **
+ ***************************************************************/
+
 #include "c_gaf.h"
+#include "dlstorm.h"
+
+#define GAFMAKEVERSION "5.0.0"
 
 void print_help(void);
